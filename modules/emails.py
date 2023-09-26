7b3dffeb1d96271b4f6f677c7a73e09c98a4ab03
TheCookieGorilla <114949749+TheCookieGorilla@users.noreply.github.com>
2023-09-26 07:39:59 +0200

    Update emails.py

--- modules/emails.py
+++ modules/emails.py
@@ -14,17 +14,17 @@ class Colores:
   amarillo="\033[33m"
 
 def main():
-    email = input(f'\n{Colores.azul}[~] Ingresa el email: ')
+    email = input(f'\n{Colores.azul}[~] Enter an email: ')
     if email == "" or email == " ":
-        print(f'\n{Color.red}[!] Error debes de ingresar un email.')
+        print(f'\n{Color.red}[!] Error you must enter an email.')
         time.sleep(2)
         main()
     else:
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         if (re.fullmatch(regex, email)):
-            print(f'\n{Colores.verde}[?] Email valido: ✔️')
+            print(f'\n{Colores.verde}[?] Email valid: ✔️')
         else:
-            print(f'\n{Colores.red}[!] Email invalido')
+            print(f'\n{Colores.red}[!] Email invalid')
             exit()
     dom = ["com","com.tw","co.in","be","de","co.uk","co.ma","dz","ru","ca"]
     tld = random.choice(dom)
@@ -35,21 +35,21 @@ def main():
     command5 = f"site:instagram.com intext:{email}"
     for j in search(command, tld, num=10, stop=10, pause=2):
         print(f'\nResultados encontrados!: {j}')
-    print(f'\n{Colores.azul}[~] Buscando email en archivos pdf...')
+    print(f'\n{Colores.azul}[~] Searching email in pdf files...')
     time.sleep(3)
     for i in search(command2, tld, num=10, stop=10, pause=2):
         print(f'\nResultados encontrados!: {i}')
-    print(f'\n{Colores.amarillo}[~] Buscando correo electronico en redes sociales...')
+    print(f'\n{Colores.amarillo}[~] Searching for email on social networks...')
     for a in search(command3, tld, num=10, stop=10, pause=2):
         print(f'\nResultados encontrados!: {a}')
     for b in search(command4, tld, num=10, stop=10, pause=2):
       print(f'\nResultados encontrados!: {b}')
     for c in search(command5, tld, num=10, stop=10, pause=2):
       print(f'\nResultados encontrados!: {c}')
-    print(f'\n{Colores.verde}[~] Iniciando socialscan...')
+    print(f'\n{Colores.verde}[~] Starting socialscan...')
     time.sleep(3)
     print('')
     os.system(f"socialscan {email}")
-    print(f'\n{Colores.azul}[~] Escaneo completo.')
+    print(f'\n{Colores.azul}[~] Scanning Complete.')
 
 main()
